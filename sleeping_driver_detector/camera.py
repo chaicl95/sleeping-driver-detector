b46e2f097557d472beae4f9f006f622a546ee364
chaicl95 <chaicl9504@gmail.com>
2023-02-01 17:48:31 +0800

    Improve Camera implementation

--- sleeping_driver_detector/camera.py
+++ sleeping_driver_detector/camera.py
@@ -1,17 +1,31 @@
 import cv2 as cv
-import numpy as np
+from typing import Tuple
 
 class Camera:
-    def __init__(self) -> None:
-        self.cap = cv.VideoCapture(0)
+    def __init__(self, video_source =0) -> None:
+        self.cap = cv.VideoCapture(video_source)
         if not self.cap.isOpened():
             raise Exception("Camera is not initialized")
+        self.height = self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+        self.width = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)
         
-    def read(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            raise Exception("Can't receive frame")
-        return frame
+    def get_frame(self):
+        ret = 0
+        if self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                return (ret, frame)
+            return (ret, None)
+        return (ret, None)
+        
+    def __del__(self):
+        if self.cap.isOpened():
+            self.cap.released()
+    
+
+
+            
+            
 
     
 
